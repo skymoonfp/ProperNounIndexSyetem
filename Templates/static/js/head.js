9e9041d07503b56561b0c82c7bc5fea7efa0ae48
心齋 <954918@qq.com>
2019-06-04 20:53:14 +0800

    Initial commit
    Signed-off-by: 心齋 <954918@qq.com>

--- Templates/static/js/head.js
+++ Templates/static/js/head.js
@@ -16,3 +16,27 @@ Go();
 function Stop() {
     clearInterval(interval);
 }
+
+
+/*//定义setTimeout执行方法
+var time = null;
+
+$('#full_2').click(function () {
+    // 取消上次延时未执行的方法
+    Stop();
+    clearTimeout(time);
+    //执行延时
+    time = setTimeout(function(){
+        //do function在此处写单击事件要执行的代码
+        Go();
+    },300);
+});
+
+$('#full_2').dblclick(functin () {
+    // 取消上次延时未执行的方法
+    Go();
+    clearTimeout(time);
+    //双击事件的执行代码
+    Stop();
+});*/
+
