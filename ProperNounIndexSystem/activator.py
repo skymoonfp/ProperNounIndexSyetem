d33f74a3deefc6a68ad4b9a82d7ba3f5ab12ef11
心齋 <954918@qq.com>
2019-06-12 11:44:13 +0800

    Initial commit
    Signed-off-by: 心齋 <954918@qq.com>

--- ProperNounIndexSystem/activator.py
+++ ProperNounIndexSystem/activator.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python
-# coding:utf-8
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 
 from django.shortcuts import HttpResponse, redirect
 
 
 def process(request, **kwargs):
-    ''' 接收所有匹配url的请求，根据请求url中的参数，通过反射动态指定view中的方法 '''
+    """
+    接收所有匹配url的请求，根据请求url中的参数，通过反射动态指定view中的方法
+    :param request:
+    :param kwargs:
+    :return:
+    """
 
     app = kwargs.get('app', None)
     function = kwargs.get('function', None)
@@ -22,10 +27,11 @@ def process(request, **kwargs):
     except (ImportError, AttributeError) as e:
         # 导入失败时，自定义404错误
         print(e)
-        return HttpResponse('404 Not Found')
+        return HttpResponse('404 Not Found：', e)
     except Exception as e:
         print(Exception, e)
         # 代码执行异常时，自动跳转到指定页面
         return redirect('/Main/index/')
+        # return HttpResponse('代码执行异常：'.encode("gbk"), e)
 
     return result
--- Templates/static/js/searchEngine.js
+++ Templates/static/js/searchEngine.js
@@ -1,30 +1,31 @@
-function SearchEngine() {
-    var se_name = $("input[name='search']:checked").val();
+function SearchEngine(form_id, input_id) {
+    var se_name = $("input[name='search_engine']:checked").val();
+    //var se_name = $(id).parent().find("input[name='search_engine']:checked").val()
 
     if (se_name == "baidu") {
         var se_url = "https://www.baidu.com/s?";
-        var url_id = document.getElementById('fsearch');
+        var url_id = document.getElementById(form_id);
         url_id.action = se_url
         var se_key = "wd";
-        var key_id = document.getElementById('search_1');
+        var key_id = document.getElementById(input_id);
         key_id.name = se_key;
     }
 
     else if (se_name == "google") {
         var se_url = "https://www.google.com.hk/search?";
-        var url_id = document.getElementById('fsearch');
+        var url_id = document.getElementById(form_id);
         url_id.action = se_url
         var se_key = "q";
-        var key_id = document.getElementById('search_1');
+        var key_id = document.getElementById(input_id);
         key_id.name = se_key;
     }
 
     else if (se_name == "sogou") {
         var se_url = "https://www.sogou.com/tx?";
-        var url_id = document.getElementById('fsearch');
+        var url_id = document.getElementById(form_id);
         url_id.action = se_url
         var se_key = "query";
-        var key_id = document.getElementById('search_1');
+        var key_id = document.getElementById(input_id);
         key_id.name = se_key;
     }
 }
